1df7524b6d65fd04ca217c363ce5da60a8aa11b4
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2017-10-04 12:54:24 +0200

    Add parallelization

--- run_simulations_fig_1.R
+++ run_simulations_fig_1.R
@@ -1,15 +1,35 @@
-source('runLFP.R') #load functions
-celltype <- 1 #Ball and stick
-path <- 'simulation' 
-lfpysim <- 4 #simulate oscillations
-eldistribute <- 1 #grid
-orientation <-2 #y-axis
-cellelectrodedist <- 50
-colnb <- 1 #one column
-
-for (rownb in c(8,16,128)){
+library(parallel)
+runOneBS<-function(rownb){
+    source('runLFP.R') #load functions
+    source('runKernel.R')
     
-    cellname <- paste("BS_d",cellelectrodedist,"el",rownb,"_CosChanging")
+    celltype <- 1 #Ball and stick
+    path <- 'simulation' 
+    lfpysim <- 4 #simulate oscillations
+    eldistribute <- 1 #grid
+    orientation <-2 #y-axis
+    cellelectrodedist <- 50
+    colnb <- 1 #one column
+    
+    dir = getwd()
+    
+    cellname <- paste("BS_d",cellelectrodedist,"el",rownb,"_CosChanging",sep="")
 
     LFPy_run(cellname,celltype,path,lfpysim,eldistribute,orientation,colnb,rownb,cellelectrodedist,xmin=-100,xmax=600,ymin=0,ymax=200,ssNb=123456,triside=19)
+    path = paste('simulation/',cellname,sep="")
+    kernel_calculate(path)
+    setwd(dir)
+
 }
+
+
+
+# Calculate the number of cores
+no_cores <- detectCores() - 1
+
+# Initiate cluster
+cl <- makeCluster(no_cores)
+ele = c(8,16,128)
+parLapply(cl,ele,runOneBS)
+
+
