7a976d1ac9b36a1111cbf11cccd4093f806735fe
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2017-10-17 15:33:35 +0200

    Script for running Fig 1

--- run_simulations_fig_1.R
+++ run_simulations_fig_1.R
@@ -1,35 +1,27 @@
-library(parallel)
-runOneBS<-function(rownb){
-    source('runLFP.R') #load functions
-    source('runKernel.R')
-    
-    celltype <- 1 #Ball and stick
-    path <- 'simulation' 
-    lfpysim <- 4 #simulate oscillations
-    eldistribute <- 1 #grid
-    orientation <-2 #y-axis
-    cellelectrodedist <- 50
-    colnb <- 1 #one column
-    
-    dir = getwd()
+source('runLFP.R') #load functions
+source('runKernel.R')
+
+celltype <- 1 #Ball and stick
+abspath <- 'simulation' 
+lfpysim <- 6 #simulate oscillations
+eldistribute <- 1 #grid
+orientation <-2 #y-axis
+cellelectrodedist <- 50
+colnb <- 1 #one column
+
+dir = getwd()
+for (rownb in c(8,16,128)){
     
     cellname <- paste("BS_d",cellelectrodedist,"el",rownb,"_CosChanging",sep="")
 
-    LFPy_run(cellname,celltype,path,lfpysim,eldistribute,orientation,colnb,rownb,cellelectrodedist,xmin=-100,xmax=600,ymin=0,ymax=200,ssNb=123456,triside=19)
+    LFPy_run(cellname,celltype,abspath,lfpysim,eldistribute,orientation,colnb,rownb,cellelectrodedist,xmin=-100,xmax=600,ymin=0,ymax=200,ssNb=123456,triside=19)
+    cat('Run LFP')
+    setwd(dir)
     path = paste('simulation/',cellname,sep="")
+    #source('simulation/BS_cos_simulation.R')
+    cat('simulation')
     kernel_calculate(path)
     setwd(dir)
-
 }
 
-
-
-# Calculate the number of cores
-no_cores <- detectCores() - 1
-
-# Initiate cluster
-cl <- makeCluster(no_cores)
-ele = c(8,16,128)
-parLapply(cl,ele,runOneBS)
-
-
+source('Figures/Fig1_BallStickOsc/BSCosPlot.R')
